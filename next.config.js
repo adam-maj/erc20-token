ea3170af53b729808b5c9cda011ec56f728a0fd1
adam-maj <mr.adam.maj@gmail.com>
2021-03-03 18:19:02 -0500

    Remove next config from tracking

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  env: {
-    tokenContractAddress: '0x747595C11aB1752a246265Da5a47002A7521A3Fb',
-    tokenSaleContractAddress: '0xF2d38DDD442EC7E23720455Fe9554AF35fD7f392',
-  }
-}
